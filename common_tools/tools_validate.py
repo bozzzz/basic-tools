dbe04ee35f72bca5655c403e8eac3330e0833ce2
Bozena Lukasiak/Content Capsules (BXS) /SRPOL/Professional/삼성전자 <b.lukasiak@amdc1051.eu.corp.samsungelectronics.net>
2018-12-19 12:28:30 +0100

    validation for equal class division

--- common_tools/tools_validate.py
+++ common_tools/tools_validate.py
@@ -10,7 +10,6 @@ import random
 import importlib
 import numpy as np
 
-    
 #in house github:
 
 #import tools
@@ -22,19 +21,93 @@ importlib.reload(tools_arrays)
 
 def divide_train_test(validation, length):
     
-    if validation['type'] =='random':
-        validation['train'] = random.sample(range(length), \
-                  int(length*validation['percent_train']/100)) #
-        # randit gives repetitions which gives problems with exracting train test:
-        #np.random.randint(low=0, high=length, size=int(length*percent/100))
-        validation['train'].sort() 
-    
-    validation['test'] = list(range(0,length))
-    for i in validation['train'][::-1]:
-        validation['test'].pop(i) # works since index=number!
+    if validation['type'] =='random':       
+        validation['test'] = get_test_random(validation, validation['length'])
         
+    elif validation['type'] =='multilabel':
+        validation['test'] = get_test_multilab(validation)
+    
+    validation['train'] = get_train_from_test(validation['test'], length)
         
     return validation
+
+
+def get_train_from_test(test, length):
+
+
+    train = list(range(0,length))
+    for i in test[::-1]:
+        train.pop(i) # works since index=number!
+        
+    return train
+    
+    
+
+def get_test_random(validation, length):
+    
+    test = random.sample(range(length), int(length*validation['percent_test']/100)) 
+        # randit gives repetitions which gives problems with exracting train test:
+        #np.random.randint(low=0, high=length, size=int(length*percent/100))
+    test.sort() 
+        
+    return test
+       
+    
+def get_test_multilab(validation):
+    
+    # divide making sure that:
+    # each class is represented in the test set and training set
+    # => in the test set there is at least one item for each label => this gives 30 items! (there are repetitions)
+    labs = validation['labs']
+    classes_list = validation['classes_list']
+    test_size = int(len(labs)*validation['percent_test']/100)
+    
+    test = get_test_percent_each_class(validation, labs, classes_list)
+    
+    if len(test) > test_size: 
+        test = remove_frequent_classes(test, classes_list, len(test) - test_size)
+    elif len(test) < test_size:
+        # now we have all classes, in test set, is it enough?
+        train = get_train_from_test(test, len(labs))
+        add_to_test = random.sample(train, test_size - len(test)) 
+        test = test + add_to_test
+        
+    test = list(np.sort(test))
+
+
+    return test    
+          
+
+def get_test_percent_each_class(validation, labs, classes_list):
+    
+    test = []
+    for class_now in classes_list:
+        # for each class get in-class number of test samples equalling validation['percent_test']
+        items_in_class = tools_arrays.indices_of_value_list2dim(labs, class_now)
+        test_in_class_idx = get_test_random(validation, len(items_in_class))
+        test_in_class = tools_arrays.items_by_index(items_in_class, test_in_class_idx)
+        # test_in_class - indices of items from this class in the test set, must go to test set
+        test += test_in_class
+        
+    test = set(test)        
+    test = list(np.sort(list(set(test))))
+    
+    return test
+
+
+def remove_frequent_classes(test, classes_list, removeNo):
+    
+    print('No way, no coding after 7pm! fuck you')
+    assert(False)
+    classes_present = []
+    for i in test:
+        classes_present += classes_list[i]
+        
+    classes_present = np.sort(classes_present)
+    
+    
+    
+    return test
 
 
 def get_train_test_data(validation, data):
